f2d5685ba0dc09a55556a512a99abc9cf35d51db
Arturo Batista <batistaquiroz@gmail.com>
2024-08-04 11:44:37 -0500

    Changing Link components to Navlinks components on the header, and doing active styling

--- src/components/HeaderMenu/HeaderMenu.tsx
+++ src/components/HeaderMenu/HeaderMenu.tsx
@@ -1,29 +1,29 @@
-import * as NavigationMenu from '@radix-ui/react-navigation-menu';
-import styles from './header_menu.module.scss';
-import {Link} from 'react-router-dom';
- 
+import * as NavigationMenu from "@radix-ui/react-navigation-menu";
+import styles from "./header_menu.module.scss";
+import { NavLink } from "react-router-dom";
+
 interface NavigationMenuLinkProps {
-  href: string
+  href: string;
   children: React.ReactNode;
 }
 
-const NavigationMenuLink = ({ href, children, ...props } : NavigationMenuLinkProps) => {
+const NavigationMenuLink = ({ href, children, ...props }: NavigationMenuLinkProps) => {
   return (
-    <NavigationMenu.Link asChild>
-      <Link to={href} {...props} className={styles.NavigationMenuLink}>{children}</Link>
+    <NavigationMenu.Link asChild>  
+      <NavLink to={href} className={styles.NavigationMenuLink} {...props}> {children}</NavLink>
     </NavigationMenu.Link>
   );
-}
+};
 
 const HeaderMenu = () => {
   return (
-    <NavigationMenu.Root orientation="horizontal" className={styles.navigationMenuRoot}>
+    <NavigationMenu.Root orientation="horizontal"className={styles.navigationMenuRoot}>
       <NavigationMenu.List className={styles.navigationMenuList}>
         <NavigationMenu.Item className={styles.NavigationMenuItem}>
           <NavigationMenuLink href="/">Pokemons</NavigationMenuLink>
         </NavigationMenu.Item>
         <NavigationMenu.Item className={styles.NavigationMenuItem}>
-          <Link to="/skills" className={styles.NavigationMenuLink}>Skills</Link>
+          <NavigationMenuLink href="/skills">Skills</NavigationMenuLink>
         </NavigationMenu.Item>
       </NavigationMenu.List>
     </NavigationMenu.Root>
